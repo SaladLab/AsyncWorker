8024a2b15d68e6441727e9e81702b27dd84f9ac2
Esun Kim <veblush@gmail.com>
2016-02-26 10:00:05 +0900

    Separate WorkSyncContext from Work

--- core/AsyncWorker/Work.cs
+++ core/AsyncWorker/Work.cs
@@ -16,48 +16,6 @@ namespace AsyncWorker
         }
     }
 
-    internal class WorkSyncContext
-    {
-        public WorkSyncContext(SyncOptions options)
-        {
-            Options = options;
-            WaitingCount = options.Workers.Length + 1;
-            foreach (var worker in Options.Workers)
-            {
-                worker.QueueSync(this);
-            }
-        }
-
-        public void OnOwnerEnter(Worker worker)
-        {
-            Owner = worker;
-            if (Interlocked.Decrement(ref WaitingCount) == 0)
-            {
-                Owner.OnSyncStart(this);
-            }
-        }
-
-        public void OnWaiterEnter(Worker worker)
-        {
-            if (Interlocked.Decrement(ref WaitingCount) == 0)
-            {
-                Owner.OnSyncStart(this);
-            }
-        }
-
-        public void OnComplete()
-        {
-            foreach (var worker in Options.Workers)
-            {
-                worker.OnSyncComplete(this);
-            }
-        }
-
-        public Worker Owner;
-        public SyncOptions Options;
-        public int WaitingCount;
-    }
-
     // Work with Action
     internal class WorkA : Work
     {
---
+++ core/AsyncWorker/WorkSyncContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncWorker
+{
+    internal class WorkSyncContext
+    {
+        public WorkSyncContext(SyncOptions options)
+        {
+            Options = options;
+            WaitingCount = options.Workers.Length + 1;
+            foreach (var worker in Options.Workers)
+            {
+                worker.QueueSync(this);
+            }
+        }
+
+        public void OnOwnerEnter(Worker worker)
+        {
+            Owner = worker;
+            if (Interlocked.Decrement(ref WaitingCount) == 0)
+            {
+                Owner.OnSyncStart(this);
+            }
+        }
+
+        public void OnWaiterEnter(Worker worker)
+        {
+            if (Interlocked.Decrement(ref WaitingCount) == 0)
+            {
+                Owner.OnSyncStart(this);
+            }
+        }
+
+        public void OnComplete()
+        {
+            foreach (var worker in Options.Workers)
+            {
+                worker.OnSyncComplete(this);
+            }
+        }
+
+        public Worker Owner;
+        public SyncOptions Options;
+        public int WaitingCount;
+    }
+}
